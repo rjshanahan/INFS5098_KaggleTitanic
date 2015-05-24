1d9ab8f1292d201b0dc819fd1f653171d53a8334
Richard Shanahan <rjshanahan@gmail.com>
2015-05-24 16:21:27 +1000

    Update 03_Titanic_Model.R

--- 03_Titanic_Model.R
+++ 03_Titanic_Model.R
@@ -6,6 +6,11 @@ library(randomForest)
 library(rpart)
 library(Hmisc)
 library(party)
+library(ggplot2)
+
+#set theme for plots
+theme = theme_set(theme_minimal())
+theme = theme_update(legend.position="top")
 
 #Richard Shanahan  
 #https://github.com/rjshanahan  
@@ -13,79 +18,8 @@ library(party)
 
 #INFS 5098: PROJECT: R code for various prediction models
 
-############ 1. run prediction model using 'rpart' package ###############
 
-##USE RPART
-#remove cap on partitions
-fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + 
-               Embarked + title + familysize + nickname + altname + iceberg + deck +
-               subclass + faregroup + classregion + childage + marriagelength, 
-             data=droplevels(titanic_train), 
-             method="class",
-             control=rpart.control(minsplit=2, cp=0))
-fit
-
-#trim trees options
-#new.fit <- prp(fit,snip=TRUE)$obj
-
-#pretty randomforest plot - expensive to run
-#library(rattle)
-#library(rpart.plot)
-#library(RColorBrewer)
-
-#fancyRpartPlot(fit)             #generate plot
-
-# model execution
-Prediction <- predict(fit, titanic_test)                      #for rpart and randomforest
-
-# create dataframe and submission file for Kaggle
-submit <- data.frame(PassengerId = titanic_test$PassengerId, Survived = as.vector(Prediction))
-write.csv(submit, file = "kagglesubmission_rpart.csv", row.names = FALSE)
-
-############ 2. run prediction model using 'randomForest' package ###############
-
-#maintain this for reproducibility
-set.seed(499)
-
-#submission 3 0.76555 with recoded title
-fit <- randomForest(droplevels(as.factor(Survived)) ~ Pclass + Sex + Age + SibSp + Parch + Fare + 
-                        Embarked + title + familysize + nickname + altname + iceberg + deck +
-                        subclass + faregroup + classregion + 
-                        farepp + marriagelength + childage + side, 
-                      data=titanic_train, 
-                      importance=TRUE, 
-                      ntree=2000)
-
-#plot results to determine variable importance
-varImpPlot(fit)
-
-#ACCURACY: how worse the model performs without each variable, so a high decrease in accuracy would be expected for very predictive variables
-#GINI: measures how pure the nodes are at the end of the tree. Again it tests to see the result if each variable is taken out and a high score means the variable was important.
-
-#code to generate prettier variable importance plot
-varImpdf <- data.frame(var = factor(names(fit$variable.importance)),
-                       imp = fit$variable.importance,
-                       row.names=NULL)
-
-ggplot(data = varImpdf,
-       aes(x=reorder(varImpdf$var, -varImpdf$imp),
-           y=imp,
-           fill=var)) +
-  #scale_x_discrete(limits = varorder) +
-  xlab("Titanic Attribute - note: lowercase vars are produced through feature selection/engineering") + 
-  ylab("Variable Importance") + 
-  geom_bar(stat="identity") +
-  ggtitle("Kaggle Titanic: Variable Importance Ratings using 'randomForest'")
-
-# model execution
-Prediction <- predict(fit, titanic_test)                      #for rpart and randomforest
-
-# create dataframe and submission file for Kaggle
-submit <- data.frame(PassengerId = titanic_test$PassengerId, Survived = as.vector(Prediction))
-write.csv(submit, file = "kagglesubmission_randomforest.csv", row.names = FALSE)
-
-############ 3. run prediction model using 'ctree' package ###############
-library(party)
+############ 1. run prediction model using 'ctree' package ###############
 
 fit <- ctree(droplevels(Survived) ~ Pclass + Sex + Age + SibSp + Parch + Fare + 
                Embarked + titlegroup + familysize + nickname + altname + iceberg + deck +
@@ -108,7 +42,7 @@ submit <- data.frame(PassengerId = titanic_test$PassengerId, Survived = as.vecto
 write.csv(submit, file = "kagglesubmission_ctree.csv", row.names = FALSE)
 
 
-############ 4. LOGIT model and generate output ###############
+############ 2. LOGIT model and generate output ###############
 
 # create variable vectors for inclusion in model
 var_exist <- c(
@@ -177,3 +111,90 @@ Prediction <- ifelse(titanic_test$pred < 0.5,
 submit <- data.frame(PassengerId = titanic_test$PassengerId, Survived = as.vector(Prediction))
 
 write.csv(submit, file = "kagglesubmission_logit.csv", row.names = FALSE)
+
+
+############ 3. run prediction model using 'randomForest' package ###############
+
+#maintain this for reproducibility
+set.seed(499)
+
+#submission 3 0.76555 with recoded title
+fit <- randomForest(droplevels(as.factor(Survived)) ~ Pclass + Sex + Age + SibSp + Parch + Fare + 
+                      Embarked + title + familysize + nickname + altname + iceberg + deck +
+                      subclass + faregroup + classregion + 
+                      farepp + marriagelength + childage + side, 
+                    data=titanic_train, 
+                    importance=TRUE, 
+                    ntree=2000)
+
+#plot results to determine variable importance - default from package
+varImpPlot(fit)
+
+#code to generate prettier variable importance plot
+varImpdf <- data.frame(var = factor(row.names(importance(fit))),
+                       imp = importance(fit)[,4],
+                       row.names=NULL)
+
+ggplot(data = varImpdf,
+       aes(x=reorder(varImpdf$var, -varImpdf$imp),
+           #x=var,
+           y=imp,
+           fill=var)) +
+  #scale_x_discrete(limits = varorder) +
+  xlab("Titanic Attribute - note: lowercase vars are produced through feature selection/engineering") + 
+  ylab("Variable Importance") + 
+  geom_bar(stat="identity") +
+  ggtitle("Kaggle Titanic: Variable Importance Ratings using 'randomForest'")
+
+
+
+#ACCURACY: how worse the model performs without each variable, so a high decrease in accuracy would be expected for very predictive variables
+#GINI: measures how pure the nodes are at the end of the tree. Again it tests to see the result if each variable is taken out and a high score means the variable was important.
+
+# model execution
+Prediction <- predict(fit, titanic_test)                      #for rpart and randomforest
+
+# create dataframe and submission file for Kaggle
+submit <- data.frame(PassengerId = titanic_test$PassengerId, Survived = as.vector(Prediction))
+write.csv(submit, file = "kagglesubmission_randomforest.csv", row.names = FALSE)
+
+############ 4. run prediction model using 'rpart' package ###############
+
+##USE RPART
+#remove cap on partitions
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + 
+               Embarked + title + familysize + nickname + altname + iceberg + deck +
+               subclass + faregroup + classregion + childage + marriagelength, 
+             data=droplevels(titanic_train), 
+             method="class",
+             control=rpart.control(minsplit=2, cp=0))
+fit
+
+
+#code to generate prettier variable importance plot
+varImpdf <- data.frame(var = factor(names(fit$variable.importance)),
+                       imp = fit$variable.importance,
+                       row.names=NULL)
+
+ggplot(data = varImpdf,
+       aes(x=reorder(varImpdf$var, -varImpdf$imp),
+           #x=var,
+           y=imp,
+           fill=var)) +
+  xlab("Titanic Attribute - note: lowercase vars are produced through feature selection/engineering") + 
+  ylab("Variable Importance") + 
+  geom_bar(stat="identity") +
+  ggtitle("Kaggle Titanic: Variable Importance Ratings using 'RPART'")
+
+
+#specify levels for 'title' as differs between test and train
+levels(titanic_test$title) <- unique(titanic_combi$title)
+
+# model execution
+Prediction <- predict(fit, titanic_test)                 
+
+# create dataframe and submission file for Kaggle
+submit <- data.frame(PassengerId = titanic_test$PassengerId, Survived = as.vector(round(Prediction[,2], digits=0)))
+write.csv(submit, file = "kagglesubmission_rpart.csv", row.names = FALSE)
+
+
